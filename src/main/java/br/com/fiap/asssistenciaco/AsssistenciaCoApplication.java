dcefb047a318dbc575e67a56e62f3d0610d009a8
Tatiane Mendonça <97405991+Tati-Mendonca@users.noreply.github.com>
2023-09-11 20:20:05 -0300

    h2 database

---
+++ src/main/java/br/com/fiap/asssistenciaco/AsssistenciaCoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.asssistenciaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsssistenciaCoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsssistenciaCoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/asssistenciaco/AsssistenciaCoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.asssistenciaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsssistenciaCoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
